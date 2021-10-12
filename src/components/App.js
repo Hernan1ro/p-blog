442ab42bb1ac17e50a8e591fd9dd4f284d26ab6c
Hernán D. Mercado <hernandmf@gmail.com>
2021-10-12 08:12:49 -0500

    react router

--- src/components/App.js
+++ src/components/App.js
@@ -1,47 +1,17 @@
 import React from "react";
-import axios from "axios";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Usuarios from "./Usuarios";
+import Menu from "./Menu";
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      usuarios: [],
-    };
-  }
-  async componentDidMount() {
-    const response = await axios.get(
-      "https://jsonplaceholder.typicode.com/users"
-    );
-    this.setState({
-      usuarios: response.data,
-    });
-    console.log(response.data);
-  }
-  renderList() {
-    return this.state.usuarios.map((user) => (
-      <tr key={user.id}>
-        <td>{user.name}</td>
-        <td>{user.email}</td>
-        <td>{user.website}</td>
-      </tr>
-    ));
-  }
-  render() {
-    return (
-      <div className="margen">
-        <table className="tabla">
-          <thead>
-            <tr>
-              <th>Nombre</th>
-              <th>Correo</th>
-              <th>Enlace</th>
-            </tr>
-          </thead>
-          <tbody>{this.renderList()}</tbody>
-        </table>
-      </div>
-    );
-  }
-}
+const App = () => {
+  const Tareas = () => <div>Tareas</div>;
+  return (
+    <BrowserRouter>
+      <Menu />
+      <Route exact path="/" component={Usuarios} />
+      <Route exact path="/tareas" component={Tareas} />
+    </BrowserRouter>
+  );
+};
 
 export default App;
---
+++ src/components/Usuarios/index.js
@@ -0,0 +1,47 @@
+import React from "react";
+import axios from "axios";
+
+class Usuarios extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      usuarios: [],
+    };
+  }
+  async componentDidMount() {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+    this.setState({
+      usuarios: response.data,
+    });
+    console.log(response.data);
+  }
+  renderList() {
+    return this.state.usuarios.map((user) => (
+      <tr key={user.id}>
+        <td>{user.name}</td>
+        <td>{user.email}</td>
+        <td>{user.website}</td>
+      </tr>
+    ));
+  }
+  render() {
+    return (
+      <div className="margen">
+        <table className="tabla">
+          <thead>
+            <tr>
+              <th>Nombre</th>
+              <th>Correo</th>
+              <th>Enlace</th>
+            </tr>
+          </thead>
+          <tbody>{this.renderList()}</tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default Usuarios;
