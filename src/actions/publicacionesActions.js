a7963e8577e7e9a2b6784f2a46259f50366adbe3
Hernán D. Mercado <hernandmf@gmail.com>
2021-10-15 08:56:45 -0500

    inmutability

--- src/actions/publicacionesActions.js
+++ src/actions/publicacionesActions.js
@@ -4,6 +4,9 @@ import {
   ERROR,
   CARGANDO,
 } from "../types/publicacionesTypes";
+import * as usuariosTypes from "../types/usuariosTypes";
+
+const { TRAER_TODOS: USUARIOS_TRAER_TODOS } = usuariosTypes;
 
 export const traerPorUsuario = (key) => async (dispatch, getState) => {
   const { usuarios } = getState().usuariosReducer;
@@ -13,6 +16,17 @@ export const traerPorUsuario = (key) => async (dispatch, getState) => {
     `http://jsonplaceholder.typicode.com/posts?userId=${usuario_id}`
   );
   const publicaciones_actualizadas = [...publicaciones, respuesta.data];
+
+  const publicaciones_key = publicaciones_actualizadas.length - 1;
+  const usuarios_actualizados = [...usuarios];
+  usuarios_actualizados[key] = {
+    ...usuarios[key],
+    publicaciones_key,
+  };
+  dispatch({
+    type: USUARIOS_TRAER_TODOS,
+    payload: usuarios_actualizados,
+  });
   dispatch({
     type: TRAER_POR_USUARIO,
     payload: publicaciones_actualizadas,
