f1ab0eb7694b418a9e16cd6943ab83486b260d89
Hernán D. Mercado <hernandmf@gmail.com>
2021-10-16 10:45:49 -0500

    data normalitation

--- src/actions/tareasActions.js
+++ src/actions/tareasActions.js
@@ -10,9 +10,20 @@ export const traerTareas = () => async (dispatch) => {
     const respuesta = await axios.get(
       "https://jsonplaceholder.typicode.com/todos"
     );
+    const tareas = {};
+    respuesta.data.map(
+      (tar) =>
+        (tareas[tar.userId] = {
+          ...tareas[tar.userId],
+          [tar.id]: {
+            ...tar,
+          },
+        })
+    );
+
     dispatch({
       type: TRAER_TAREAS,
-      payload: respuesta.data,
+      payload: tareas,
     });
   } catch (error) {
     dispatch({
